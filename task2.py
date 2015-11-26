26c97900d0295518e9721a44c0f97c1d3f1077c1
Tatu Seppä-Lassila <tatu.seppa.lassila@gmail.com>
2015-11-26 12:28:52 +0200

    Small improvements

--- task2.py
+++ task2.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import math
 import os
 import pandas
+import itertools
 from pandas.tools.plotting import parallel_coordinates
 from whiteWineData import WhiteWineData
 
@@ -23,12 +24,24 @@ def drawParallelCoordinates(data, columnString):
 	plt.show()
 
 def drawParallelCoordinatesWithLimit(data, columnString):
+	# Reduce number of samples
+	#sampledData = data.loc[np.random.choice(data.index, 500, replace=False)]
+	#print(len(sampledData))
+	normData = data/data.max().astype(np.float64)
 	plt.figure()
-	parallel_coordinates(data, columnString)
-	plt.ylim(0, 10)
+	parallel_coordinates(normData, columnString)
+	#plt.ylim(0, 15)
 	plt.show()
 
 #drawParallelCoordinates(data2, 'quality')
-drawParallelCoordinatesWithLimit(data2, 'quality')
+#drawParallelCoordinatesWithLimit(data2, 'quality')
+#drawParallelCoordinates(data2, 'quality')
 
 #drawScatterPlot(data.density, data.fixedAcidity, 'Density', 'Fixed Acidity')
+
+# Gets all possible attribute pairs
+'''
+attrComb = list(itertools.combinations(data1.attributeNames, 2))
+for comb in attrComb:
+	print(comb)
+'''
