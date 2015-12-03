f2a07838b44adbe05bae274a308a34db19248aa3
Tatu Seppä-Lassila <tatu.seppa.lassila@gmail.com>
2015-12-03 10:27:20 +0200

    Started task1 of exercise work 2

--- exerciseWork2/task1.py
+++ exerciseWork2/task1.py
@@ -1,6 +1,23 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 
+# Read data to pandas dataframe
 data = pd.read_csv('winequality-white.csv', sep=';')
 
-print(data)
+
+# Select input variables as x
+x = data.iloc[0:,0:11]
+
+# Select output variable (quality) as y
+y = data.quality
+
+print(x)
+
+'''
+lm = LinearRegression()
+
+lm.fit(x,y)
+
+print(lm.intercept_)
+'''
