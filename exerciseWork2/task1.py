bf66a100a0e355541291b44fdfcb7e406959d79d
Tatu Seppä-Lassila <tatu.seppa.lassila@gmail.com>
2015-12-09 17:15:52 +0200

    Continued task1

--- exerciseWork2/task1.py
+++ exerciseWork2/task1.py
@@ -5,19 +5,49 @@ from sklearn.linear_model import LinearRegression
 # Read data to pandas dataframe
 data = pd.read_csv('winequality-white.csv', sep=';')
 
-
 # Select input variables as x
 x = data.iloc[0:,0:11]
 
 # Select output variable (quality) as y
 y = data.quality
 
-print(x)
-
-'''
+# Use linear regression from scikit
 lm = LinearRegression()
 
 lm.fit(x,y)
 
-print(lm.intercept_)
-'''
+# Get intercept and coefficients
+coefs = lm.coef_
+intercept = lm.intercept_
+
+# Combine them to get the weight vector
+weightVector = np.hstack((np.array(intercept), np.array(coefs)))
+
+print('Coeffients are: ')
+
+for i in zip(x, coefs):
+	print(i)
+
+splittedData = np.array_split(data, 5)
+
+# Calculate out sample error
+
+
+outOfSampleErrorVectors = []
+
+for part in splittedData:
+	splittedX = part.iloc[0:,0:11]
+	splittedY = part.quality
+	predictedQualityList = []
+
+	for sample in splittedX.iterrows():
+		numpySample = np.array(sample[1])
+		#print(numpySample)
+		predictedQualityList.append(lm.predict(numpySample))
+	
+	outOfSampleVector = []
+ 
+	for i in range(0, len(splittedY)):
+		print(splittedY[i][0] - predictedQualityList[i])
+		#outOfSampleVector.append(splittedY[i] - predictedQualityList[i])
+	
