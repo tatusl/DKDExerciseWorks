598eaeb92272f9480ea12dec36560efc7fcf3d63
Tatu Seppä-Lassila <tatu.seppa.lassila@gmail.com>
2015-12-11 17:16:51 +0200

    Finished task1

--- exerciseWork2/task1.py
+++ exerciseWork2/task1.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import itertools
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 
@@ -28,6 +29,45 @@ def calculateOutOfSampleErrorVectors(dataIn):
 
 	return outOfSampleErrorVectors
 
+# Returns in-sample error vectors
+def calculateInSampleErrorVectors(dataIn):
+	# Split data to 5 parts
+	splittedData = np.array_split(dataIn, 5)
+	
+	# Get index combinations
+	indexCombinations = list(itertools.combinations(range(0, len(splittedData)), 4))
+
+	# Create in-sample data
+	inSampleDataList = []
+
+	for combination in indexCombinations:
+		inSampleData = []
+		for index in combination:
+				dataInIndex = list(np.array(splittedData[index]))
+				inSampleData += dataInIndex
+		inSampleData = np.array(inSampleData)
+		inSampleDataList.append(inSampleData)
+
+	inSampleErrorVectors = []
+
+	for dataPart in inSampleDataList:
+		# Split input variables and output variable out of the data part. Typecast to numpy array
+		splittedX = dataPart[0:,0:11]
+		splittedY = dataPart[:,11]
+
+		# List containing errors for the data part
+		errors = []
+
+		for i in range(0, len(splittedY)):
+			sampleReshaped = splittedX[i].reshape(1, -1)
+			# Subtract the predicted quality from actual quality
+			error = float(splittedY[i] - lm.predict(sampleReshaped))
+			errors.append(error)
+
+		inSampleErrorVectors.append(errors)
+
+	return inSampleErrorVectors
+
 # Returns concatenated vectors
 def concatenateVectors(vectors):
 	vectorsConcatenated = []
@@ -36,9 +76,10 @@ def concatenateVectors(vectors):
 	return vectorsConcatenated
 
 # Plot histogram
-def plotHistogram(concatVector):
+def plotHistogram(concatVector, title):
 	figure = plt.figure()
 	plt.hist(concatVector, bins=50, color='b')
+	plt.title(title)
 	plt.show()
 
 # Read data to pandas dataframe
@@ -66,9 +107,28 @@ print('Coeffients are: ')
 for i in zip(x, coefs):
 	print(i)
 
+
 outOfSampleErrorVectors = calculateOutOfSampleErrorVectors(data)
+concatenatedOut = concatenateVectors(outOfSampleErrorVectors)
+#plotHistogram(concatenatedOut, 'Out-of-Sample error histogram')
 
-concatenated = concatenateVectors(outOfSampleErrorVectors)
+inSampleErrorVectors = calculateInSampleErrorVectors(data)
+concatenatedIn = concatenateVectors(inSampleErrorVectors)
+#plotHistogram(concatenatedIn, 'In-Sample error histogram')
 
-plotHistogram(concatenated)
+numpyOut = np.array(concatenatedOut)
+meanOut = np.mean(numpyOut)
+varOut = np.var(numpyOut)
 
+numpyIn = np.array(concatenatedIn)
+meanIn = np.mean(numpyIn)
+varIn = np.var(numpyIn)
+
+print("Average error for out-of-sample: ")
+print(meanOut)
+print("Average error for in-sample: ")
+print(meanIn)
+print("Variance for out-of-sample")
+print(varOut)
+print("Variance for in-sample")
+print(varIn)
